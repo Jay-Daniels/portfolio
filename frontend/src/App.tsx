b62aaa314e67bb83c4f037fa0e4a7ca4c71649f9
Justin Daniels <justin.daniels@gmx.net>
2024-11-11 01:16:22 +0100

    feat(frontend): [#001] Set up frontend with React, TypeScript, Vite, Tailwind, Material UI, and Axios
    - Created a new frontend setup with React and TypeScript support using Vite.
    - Installed and configured Tailwind CSS for styling.
    - Added Material UI for UI components.
    - Installed Axios for making HTTP requests.
    - Created SkillDisplay component in `frontend/src/components/SkillDisplay/SkillDisplay.tsx`, utilizing React, Axios, and Tailwind.
    - Moved icons from the backend Resources folder to `frontend/public/components/skills-display/icons` for easier access.
    - Modified `App.tsx` to load the `SkillDisplay` component.
    - Updated `import.sql` for necessary database changes.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import SkillsDisplay from './components/SkillsDisplay/SkillsDisplay';
+
+const App = () => {
+  return (
+    <div className="App">
+      <SkillsDisplay />
+    </div>
+  );
+}
+
+export default App;
